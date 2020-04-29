bd5e390fffcdaa29f919a75b5e31273e68964024
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-04-29 17:13:19 -0300

    Projeto Estruturado

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function SignUp() {
+  return (
+    <View>
+        <Text>SignIn</Text>
+    </View>
+  );
+}
