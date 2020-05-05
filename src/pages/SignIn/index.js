b63a32b6a164243abf703b9e548a12840a2b9116
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-05-05 19:55:25 -0300

    Adicionando styled-components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { View, Text} from 'react-native';
 
-// import { Container } from './styles';
+import { Background } from './styles';
 
 export default function SignIn() {
   return (
-    <View>
-        <Text>SignIn</Text>
-    </View>
+    <Background>
+      
+      <Container>
+
+        <Logo source={require('../../assets/logo.png')}/>
+
+        <AreaInput>
+        
+        
+        </AreaInput>
+
+
+      </Container>
+
+    </Background>
   );
 }
