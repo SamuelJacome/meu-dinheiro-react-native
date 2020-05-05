dcfa090bfca85a77ff67daddf092428d36fce6b3
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-05-05 19:36:58 -0300

    stylizando statusBar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { StatusBar } from 'react-native';
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View>
-        <Text>
-            App
-        </Text>
-    </View>
+    <>
+        <StatusBar backgroundColor="#262630" barStyle="light-content"/>
+        <Routes/>
+    </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text} from 'react-native';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
   return (
     <View>
-        <Text>SignIn</Text>
+        <Text>SignUp</Text>
     </View>
   );
 }
