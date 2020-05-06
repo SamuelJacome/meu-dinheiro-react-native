cc30aaee567075b9aea3c56a21a98c59e941fb06
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-05-05 21:17:55 -0300

    estabilizado

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text} from 'react-native';
 
-import { Background } from './styles';
+import { Background,
+          Container,
+          Logo,
+          AreaInput,
+          Input,
+          SubmitButton,
+          SignUpLink,
+          SignUpText,
+          SubmitText
+
+
+} from './styles';
 
 export default function SignIn() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <Background>
       
@@ -12,10 +26,46 @@ export default function SignIn() {
         <Logo source={require('../../assets/logo.png')}/>
 
         <AreaInput>
+          <Input
+            placeholder="Email"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={email}
+            onChangeText={(texto)=> setEmail(texto)}
+          
+          
+          />
+        </AreaInput>
+
+
+        <AreaInput>
+
+        <Input
+            placeholder="Email"
+            autoCorrect={false}
+            autoCapitalize="none"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={(password)=> setPassword(password)}
+          
+          
+          />
         
         
         </AreaInput>
 
+
+        <SubmitButton onPress={()=>{}}>
+
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+
+        <SignUpLink onPress={()=>{}}>
+          <SignUpText>Criar Conta Gratuita</SignUpText>
+        </SignUpLink>
+
+      
 
       </Container>
 
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -3,3 +3,36 @@ import styled from 'styled-components/native';
 export const Background = styled.View`
 
 `;
+
+export const Container = styled.View`
+
+`;
+
+export const Logo = styled.Image`
+
+`;
+
+export const AreaInput = styled.View`
+
+`;
+
+export const Input = styled.TextInput`
+
+`;
+
+export const SubmitButton = styled.TouchableOpacity`
+
+`;
+
+export const SubmitText = styled.Text`
+
+`;
+
+
+export const SignUpLink = styled.TouchableOpacity`
+
+`;
+
+export const SignUpText = styled.Text`
+
+`;
